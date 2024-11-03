910bd37f113caaa5ce291f474a0c240759cb445b
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2024-11-03 21:32:15 +0100

    Create form contact component

--- src/app/pages/contacts/contact-form/contact-form.component.ts
+++ src/app/pages/contacts/contact-form/contact-form.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactForm!: FormGroup;
 
+  constructor(){
+    this.contactForm=new FormGroup({
+      name:new FormControl('',Validators.required),
+      phone:new FormControl('',Validators.required),
+      active: new FormControl(true),
+    });
+  }
+
+  get name(){
+    return this.contactForm.get('name');
+  }
+
+  get phone(){
+    return this.contactForm.get('phone');
+  }
+
+  get active(){
+    return this.contactForm.get('active');
+  }
 }
