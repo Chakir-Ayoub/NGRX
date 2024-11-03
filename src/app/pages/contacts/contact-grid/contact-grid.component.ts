d3b50580144b3966ba5f227ff38f526365dcf8f6
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2024-11-03 21:49:05 +0100

    List and Grid Components

--- src/app/pages/contacts/contact-grid/contact-grid.component.ts
+++ src/app/pages/contacts/contact-grid/contact-grid.component.ts
@@ -1,12 +1,16 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactActionsComponent } from '../contact-actions/contact-actions.component';
+import { Observable } from 'rxjs';
+import { ContactModels } from '../../../store/contact/contact.models';
 
 @Component({
   selector: 'app-contact-grid',
   standalone: true,
-  imports: [],
+  imports: [ContactActionsComponent,AsyncPipe],
   templateUrl: './contact-grid.component.html',
   styleUrl: './contact-grid.component.css'
 })
 export class ContactGridComponent {
-
+  contacts!:Observable<ContactModels[]>;
 }
--- src/app/pages/contacts/contact-list/contact-list.component.ts
+++ src/app/pages/contacts/contact-list/contact-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContactModels } from '../../../store/contact/contact.models';
+import { AsyncPipe } from '@angular/common';
+import { ContactActionsComponent } from '../contact-actions/contact-actions.component';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe,ContactActionsComponent],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
-
+  contacts!: Observable<ContactModels[]>;
 }
